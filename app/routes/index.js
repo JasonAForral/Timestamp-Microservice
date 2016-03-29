5664125731e8c1c34e1e5d95af54d428ecd2eb36
Jason Albert Forral <ajsnoface@gmail.com>
2016-03-29 01:48:12 +0000

    made it into a timestamp microservice

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,43 @@
 'use strict';
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+const path = process.cwd(),
+			moment = require('moment');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
+		.get((req, res) => {
+		  res.sendFile(path + '/public/index.html');
+		})
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+	app.route('/:time')
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+		.get((req, res) => {
+			
+			let dateString = req.params.time;
+			//let date = moment(new Date(req.params.time))
+			
+			if (isNaN(dateString)){
+				let date = moment(new Date(req.params.time))
+				if (date.isValid()){
+				  res.json({
+				  	unix: date.unix(),
+				  	natural: date.format('MMMM D, YYYY'),
+				  });
+				} else {
+					res.json({
+				  	unix: null,
+				  	natural: null,
+				  });
+				}
+			} else {
+				let date = moment.unix(dateString)
+			  res.json({
+			  	unix: date.unix(),
+			  	natural: date.format('MMMM D, YYYY'),
+			  });
+			} 
+		})
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
+	
